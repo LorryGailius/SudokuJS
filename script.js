afb43449ce94908dbe11a467df16561fca12a18d
LorryGailius <lauris.gailius@gmail.com>
2024-01-08 22:58:46 +0200

    Remove setCursorEnd function from script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         var value = sudoku.board[i];
         var element =
           value == null
-            ? `<input id="${i}" class="input cell" type="number" maxlength="1" onfocus="setCursorEnd(this);" oninput="this.value=this.value.replace(/[^0-9]/g,'');" autocomplete="off">`
+            ? `<input id="${i}" class="input cell" type="number" maxlength="1" oninput="this.value=this.value.replace(/[^0-9]/g,'');" autocomplete="off">`
             : `<div id="${i}" class="locked cell no-select">${value}</div>`;
 
         $("#board").append(element);
@@ -73,10 +73,4 @@ function showToast(message) {
   setTimeout(() => {
     container.fadeOut();
   }, 4000);
-}
-
-function setCursorEnd(input) {
-  // Does not work on Brave(Chromium)
-  var length = input.value.length;
-  input.setSelectionRange(length, length);
 }
